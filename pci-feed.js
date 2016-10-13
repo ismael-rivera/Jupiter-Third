0e59b5bc28abf011cdab5f33b63148f2c91095ee
Ismael Ishy Rivera <coralblue79@gmail.com>
2016-10-13 12:00:00 +0100

    Jython module for testing malware overrides in UDP interface systems

---
+++ pci-feed.js
@@ -0,0 +1,15 @@
+This is just a prop for testing out encryption. Most of the stuff here is for the use of another project which will be kept out of public server. Example list to get going:
+
+-TV Shows:
+-----------------------------------------
+Fear the Living Dead s02 - ended
+The Big Bang Theory 
+Marvels Agents of Shield Season 4 
+Wildwest
+The Strain
+Ash vs Evil Dead s02
+The Walking Dead
+
+Here's some garble:
+
+Minim veniam quis nostrud, exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea. In iis qui, facit eorum claritatem Investigationes demonstraverunt. Litterarum formas humanitatis per seacula quarta decima et quinta decima eodem modo typi qui nunc. Zzril delenit augue duis dolore te feugait nulla facilisi nam liber tempor cum. Qui sequitur mutationem consuetudium lectorum mirum est notare quam littera gothica. Tincidunt ut laoreet dolore magna aliquam erat volutpat ut wisi enim ad. Sed diam nonummy nibh euismod commodo consequat duis autem. Claritatem insitam est usus legentis lectores legere me lius quod ii legunt saepius claritas. Qui blandit praesent luptatum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim. Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim.
